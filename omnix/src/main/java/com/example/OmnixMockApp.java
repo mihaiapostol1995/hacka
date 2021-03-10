2a93efc626c8aa127d0a9cccc38bd52e4e611431
mapostol <mihai.apostol@cognyte.com>
2021-03-10 10:03:37 +0200

    logging

--- omnix/src/main/java/com/example/OmnixMockApp.java
+++ omnix/src/main/java/com/example/OmnixMockApp.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-		//(exclude = {DataSourceAutoConfiguration.class })
+		(exclude = {DataSourceAutoConfiguration.class })
 @EnableFeignClients
 public class OmnixMockApp {
 
